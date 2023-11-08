428a30244b508c410986414400151a746fb2090a
CharlyISC <jhonygenial12@gmail.com>
2023-11-07 19:16:42 -0600

    7 Nov Revision

--- APITESCHI/APITESCHI/settings.py
+++ APITESCHI/APITESCHI/settings.py
@@ -125,3 +125,16 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Servidor del correo
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'jhonygenial12@gmail.com'
+EMAIL_HOST_PASSWORD = 'ijgq najz zevo dulk'
+
+# Configuracion opcional del correo
+DEFAULT_FROM_EMAIL = 'jhonygenial12@gmail.com'
+EMAIL_SUBJECT_PREFIX = '[iBook Pro: Asignación de contraseña]'
--- APITESCHI/api/models.py
+++ APITESCHI/api/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Alumno(models.Model):
+    idAlumno = models.IntegerField(primary_key=True,db_column='idAlumno')
+    nameAlumno =models.CharField(max_length=100,db_column='nameAlumno')
+    class Meta:
+        db_table='Alumnos'
+
+
+class Libros(models.Model):
+    idLibro = models.AutoField(primary_key=True,db_column='idLibro')
+    Titulo = models.CharField(max_length=100,default='Titulo',db_column='Titulo')
+    Autor = models.CharField(max_length=60,default= 'Autor_Random',db_column='Autor')
+    Genero = models.CharField(max_length=60,default= 'Genero_Random',db_column='Genero')
+    Sinopsis = models.CharField(max_length=200,default= 'Sinopsis_Random',db_column='Sinopsis')
+    Calificacion = models.IntegerField(default='0',db_column='Calificacion')
+    class Meta:
+        db_table= 'Libros'
+
+class Biblioteca_Personal(models.Model):
+    idBiblioteca = models.AutoField(primary_key=True, db_column='idBiblioteca')
+    fk_Usuario = models.ForeignKey(User, on_delete=models.CASCADE,db_column='fk_Usuario')
+    fk_Libro = models.ForeignKey(Libros, on_delete=models.CASCADE, db_column='fk_Libro')
+    estadoLectura = models.CharField(max_length=20, default='No leido',db_column='estadoLectura')
+    class Meta:
+        db_table= 'Biblioteca_Personal'
+
+class Resenas(models.Model):
+    idResenas = models.AutoField(primary_key=True, db_column='idResenas') 
+    fk_Usuario = models.ForeignKey(User, on_delete=models.CASCADE,db_column='fk_Usuario')
+    fk_Libro = models.ForeignKey(Libros, on_delete=models.CASCADE, db_column='fk_Libro')
+    Resena = models.CharField(max_length=200, default='Resena_Random',db_column='Resena')
+    class Meta:
+        db_table= 'Resenas'
+
+class Favoritos(models.Model):
+    idFavorito = models.AutoField(primary_key=True, db_column='idFavorito')
+    fk_Usuario = models.ForeignKey(User, on_delete=models.CASCADE,db_column='fk_Usuario')
+    fk_Libro = models.ForeignKey(Libros, on_delete=models.CASCADE, db_column='fk_Libro')
+    class Meta:
+        db_table= 'Favoritos'
+
+class Encuesta(models.Model):
+    pregunta1 = models.TextField(db_column='Facilidad de uso del sistema')    
+    pregunta2 = models.TextField(db_column='Utilidad del sistema')    
+    pregunta4 = models.TextField(db_column='Frecuencia en problemas tecnicos')    
+    pregunta5 = models.TextField(db_column='Buen soporte al cliente')    
+    pregunta6 = models.TextField(db_column='Satisfaccion con actualizaciones')    
+    pregunta7 = models.TextField(db_column='Recomendacion del sistema')    
