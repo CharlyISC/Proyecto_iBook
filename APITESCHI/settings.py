7e7940ddf4d119c3af6d01a0362fddadb0cc3bd7
CharlyISC <jhonygenial12@gmail.com>
2023-11-08 18:43:30 -0600

    wpc

--- APITESCHI/settings.py
+++ APITESCHI/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vk#7er5*y)#v+-n-o8-23d^9pp=-r^174l0!hx@%lq-1i3is$1'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
